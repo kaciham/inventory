1f4f21a43ed048cec78322cad0ec103b115cf9d8
kaciham <118285179+kaciham@users.noreply.github.com>
2024-09-18 19:06:02 +0200

    db connection

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const bodyParser = require("body-parser")
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
+        console.log("DB Connected");
         app.listen(PORT, () => {
             console.log(`Server Running on http://localhost:${PORT}`);
         });
